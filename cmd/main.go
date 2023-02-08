8e29335b3c5371e9dad6fc96f6bf3ff905293b50
Matheus Bezerra <w.matheushenrique@gmail.com>
2023-02-07 23:47:40 -0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	serviceName  = "Metrics Monitor Service"
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func main() {
+	recordMetrics()
+	fmt.Printf("Listening... serviceName = %v", serviceName)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":9033", nil)
+}
