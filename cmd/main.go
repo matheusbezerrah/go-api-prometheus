f32a00f7ed90b6b5f847f10a1d6c72eb38f73199
Matheus Bezerra <w.matheushenrique@gmail.com>
2023-02-14 19:39:16 -0300

    examples of metrics

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -10,26 +9,101 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func recordMetrics() {
+var (
+	// COUNTER
+	api_num_request_post = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name:        "api_request_total",
+			Help:        "The total number of requests",
+			ConstLabels: prometheus.Labels{"method": "POST", "handler": "/message"},
+		},
+	)
+	api_num_request_get = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name:        "api_request_total",
+			Help:        "The total number of requests",
+			ConstLabels: prometheus.Labels{"method": "GET", "handler": "/message"},
+		},
+	)
+	// GAUGE
+	api_num_clients_online = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "api_clients_online",
+			Help: "The number of clients online",
+		},
+	)
+	// HISTOGRAM
+	api_response_time = promauto.NewHistogram(
+		prometheus.HistogramOpts{
+			Name:    "api_response_time_milliseconds",
+			Help:    "Response time of the api in milliseconds",
+			Buckets: prometheus.ExponentialBuckets(50, 2, 6),
+		},
+	)
+)
+
+func recordsCounterMetric() {
 	go func() {
 		for {
-			opsProcessed.Inc()
+			api_num_request_post.Inc()
+			api_num_request_post.Add(10)
+			time.Sleep(2 * time.Second)
+			api_num_request_get.Inc()
+			time.Sleep(1 * time.Second)
+		}
+	}()
+}
+
+func recordsGaugeMetric() {
+	go func() {
+		for {
+			api_num_clients_online.Add(25)
+			time.Sleep(10 * time.Second)
+		}
+	}()
+	go func() {
+		for {
+			api_num_clients_online.Dec()
 			time.Sleep(2 * time.Second)
 		}
 	}()
 }
 
-var (
-	serviceName  = "Metrics Monitor Service"
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
-	})
-)
+func recordsHistogramMetric() {
+	go func() {
+		for {
+			start := time.Now()
+			time.Sleep(45 * time.Millisecond)
+			api_response_time.Observe(float64(time.Since(start).Milliseconds()))
+		}
+	}()
+	go func() {
+		for {
+			start := time.Now()
+			time.Sleep(400 * time.Millisecond)
+			api_response_time.Observe(float64(time.Since(start).Milliseconds()))
+		}
+	}()
+	go func() {
+		for {
+			start := time.Now()
+			time.Sleep(700 * time.Millisecond)
+			api_response_time.Observe(float64(time.Since(start).Milliseconds()))
+		}
+	}()
+	go func() {
+		for {
+			start := time.Now()
+			time.Sleep(2000 * time.Millisecond)
+			api_response_time.Observe(float64(time.Since(start).Milliseconds()))
+		}
+	}()
+}
 
 func main() {
-	recordMetrics()
-	fmt.Printf("Listening... serviceName = %v", serviceName)
+	recordsCounterMetric()
+	recordsGaugeMetric()
+	recordsHistogramMetric()
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":9033", nil)
+	http.ListenAndServe(":8080", nil)
 }
